74b43607cad8578ddb12cd364b69d8b83c119b83
Vitório Espindola <vitoriomonstack@gmail.com>
2024-09-03 16:08:28 -0300

    Salvar alterações

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HelloWorld from './components/HelloWorld';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá react</h1>
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Frase.jsx
@@ -0,0 +1,10 @@
+function Frase(){
+
+    return(
+        <div>
+            <p>Este componente é com uma </p>
+        </div>
+    )
+}
+
+export default Frase
